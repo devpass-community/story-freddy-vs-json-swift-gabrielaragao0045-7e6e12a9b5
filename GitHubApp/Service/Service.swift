701578ae2859721938762f760388f8a72b985e81
gabrielaragaoo <eng.gabrielaragao@gmail.com>
2023-04-18 11:22:28 -0300

    feat: implement fetchList service method

--- GitHubApp/Service/Service.swift
+++ GitHubApp/Service/Service.swift
@@ -10,6 +10,17 @@ struct Service {
 
     func fetchList(of user: String, completion: @escaping ([Repository]?) -> Void) {
         
-        // TODO
+        guard let url = URL(string: "https://api.github.com/users/\(user)/repos") else { return }
+
+        let decoder = JSONDecoder()
+
+        network.performGet(url: url) { data in
+            do {
+                let repositories = try decoder.decode([Repository].self, from: data!)
+                completion(repositories)
+            } catch {
+                print (error.localizedDescription)
+            }
+        }
     }
 }
